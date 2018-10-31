095baac0d11a472dd5eecf51e6aa167124e62f91
Mahesh <msheshappa@sapient.com>
2018-10-31 18:26:05 +0530

    solution using for loop and es6 for of

---
+++ reverseString/solution-02.js
@@ -0,0 +1,27 @@
+function reverseStr(str) {
+    var reversedStr = '';
+    
+    for ( var i=str.length-1; i>=0; i-- ) {
+        reversedStr += str[i];
+    }
+
+    return reversedStr;
+}
+
+/* 
+ES6 for of solution 
+
+const reverseStr = (str) => {
+    let reversedStr = '';
+
+    for ( let char of str) {
+        reversedStr =  char + reversedStr;
+    }
+
+    return reversedStr;
+}
+
+*/
+
+
+
